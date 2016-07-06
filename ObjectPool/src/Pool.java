bd421a81acd6da6cefc8fc720b4fbd2267c41111
E803110 <E803110@P022614.copel.nt>
2016-07-06 17:01:10 -0300

    Object Pool + Multiton

---
+++ ObjectPool/src/Pool.java
@@ -0,0 +1,6 @@
+
+public interface Pool<T> {
+	T acquire();
+	void release(T t);
+	
+}
---
+++ ObjectPool/src/Principal.java
@@ -0,0 +1,12 @@
+public class Principal {
+	public static void main(String[] args) {
+		
+		Pool<Funcionario> funcionarioPool = new FuncionarioPool();
+		Funcionario funcionario = funcionarioPool.acquire();
+		
+		while (funcionario != null) {
+			System.out.println(funcionario.getNome());
+			funcionario = funcionarioPool.acquire();
+		}
+	}
+}
