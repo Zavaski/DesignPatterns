c1cedac2f491cfa538d4f6361d70592cdf8c8384
E803110 <E803110@P022614.copel.nt>
2016-07-08 16:24:14 -0300

    Decorator

---
+++ Decorator/src/EmissorDecorator.java
@@ -0,0 +1,13 @@
+public abstract class EmissorDecorator implements Emissor {
+	private Emissor emissor;
+
+	public EmissorDecorator(Emissor emissor) {
+		this.emissor = emissor;
+	}
+
+	public abstract void envia(String mensagem);
+
+	public Emissor getEmissor() {
+		return this.emissor;
+	}
+}
